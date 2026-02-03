ddf3d3ffcf52eb1f80a63489fc83b392fb209c51
saloumCDA03 <csgonesse@gmail.com>
2026-02-03 10:58:12 +0100

    Add files via upload

---
+++ 4-props/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WelcomeFunc from "./WelcomeFunc";
+import WelcomeClass from "./WelcomeClass";
+
+function App() {
+  return (
+    <>
+      <WelcomeFunc name="Sara" />
+      <WelcomeClass name="Sara" />
+    </>
+  );
+}
+
+export default App;
